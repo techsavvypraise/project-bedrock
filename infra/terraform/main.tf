a2c39e8954ae09e594668c21b545dddfecb8af68
Praise Sunday <108570303+techsavvypraise@users.noreply.github.com>
2025-09-17 19:41:58 +0100

    Create main.tf

---
+++ infra/terraform/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.3"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+# Placeholder - EKS and VPC resources will go here later
